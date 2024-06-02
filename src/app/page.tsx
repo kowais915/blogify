d2ef21f5c1fcb9b9be3d98f5965d60c112353fa7
Owais Khan <kowais915@gmail.com>
2024-06-03 00:30:30 +0500

    :sparkles: Coded the hero section
    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+"use client"
 import Image from "next/image";
+import Landing from '@components/pages/Landing/Landing';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        This is the main page
+    <main className="">
+        <Landing/>
     </main>
   );
 }
---
+++ src/components/pages/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+// Page components
+"use client"
+import Hero from './components/Hero';
+const LandingPage = () => {
+    return ( 
+        <>
+            <Hero/>
+        </>
+     );
+}
+ 
+export default LandingPage;
